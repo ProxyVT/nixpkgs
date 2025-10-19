204070b7483e31d4644e4ecd294e19db1524f7a3
ProxyVT <tikit.us@outlook.com>
2025-10-19 11:52:04 +0300

    Update package.nix
    Update ariang
    
    Update package.nix
    
    Revert "Update package.nix"
    
    This reverts commit 02526014ff3ac0e3aa413108eab92144adb2f09b.

--- pkgs/by-name/ar/ariang/package.nix
+++ pkgs/by-name/ar/ariang/package.nix
@@ -4,18 +4,19 @@
   buildNpmPackage,
   copyDesktopItems,
   imagemagick,
+  runtimeShell,
   xdg-utils,
   makeDesktopItem,
 }:
 
-buildNpmPackage rec {
+buildNpmPackage (finalAttrs: {
   pname = "ariang";
   version = "1.3.13";
 
   src = fetchFromGitHub {
     owner = "mayswind";
     repo = "AriaNg";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-u4MnjGMvnnb9EGHwK2QYpW7cuX1e1+6z2/1X1baR8iA=";
   };
 
@@ -32,29 +33,44 @@ buildNpmPackage rec {
     runHook preInstall
 
     mkdir -p $out/share
-    cp -r dist $out/share/${pname}
+    cp -r dist $out/share/ariang
 
-    for size in 16 24 36 48 72; do
+    for size in 16 24 32 36 48 64 72 128; do
       mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps
-      magick $out/share/${pname}/tileicon.png -resize ''${size}x''${size} \
-        $out/share/icons/hicolor/''${size}x''${size}/apps/${pname}.png
+      magick $out/share/ariang/tileicon.png -filter Lanczos -resize ''${size}x''${size} \
+        $out/share/icons/hicolor/''${size}x''${size}/apps/ariang.png
     done
 
     mkdir -p $out/bin
-    makeWrapper ${xdg-utils}/bin/xdg-open $out/bin/${pname} \
-      --add-flags "file://$out/share/${pname}/index.html"
+    cat > $out/bin/ariang << 'EOF'
+    #!${runtimeShell}
+    set -eu
+    persistent_dir="\$HOME/.local/share/ariang"
+    version_file="\$persistent_dir/.version"
+    current_version="${finalAttrs.version}"
+
+    mkdir -p "\$user_dir"
+    if [ ! -f "\$version_file" ] || [ "$(cat "\$version_file")" != "\$current_version" ]; then
+        cp -r ${placeholder "out"}/share/ariang/* "\$user_dir/"
+        echo "\$current_version" > "\$version_file"
+    fi
+
+    exec ${xdg-utils}/bin/xdg-open "file://\$user_dir/index.html"
+    EOF
+
+    chmod +x $out/bin/ariang
 
     runHook postInstall
   '';
 
   desktopItems = [
     (makeDesktopItem {
-      name = pname;
+      name = "ariang";
       desktopName = "AriaNg";
-      genericName = meta.description;
-      comment = meta.description;
-      exec = pname;
-      icon = pname;
+      genericName = finalAttrs.meta.description;
+      comment = finalAttrs.meta.description;
+      exec = "ariang";
+      icon = "ariang";
       terminal = false;
       type = "Application";
       categories = [
@@ -66,9 +82,9 @@ buildNpmPackage rec {
 
   meta = {
     description = "Modern web frontend making aria2 easier to use";
-    homepage = "http://ariang.mayswind.net/";
+    homepage = "https://ariang.mayswind.net";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ stunkymonkey ];
     platforms = lib.platforms.unix;
   };
-}
+})
