0666858a720cf3bd96b2858e418f0f0ab3590f46
ProxyVT <tikit.us@outlook.com>
2026-08-19 12:29:03 +0300

    localsend-cli: init at 1.18.1

---
+++ pkgs/by-name/lo/localsend-cli/package.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  cacert,
+  stdenv,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "localsend-cli";
+  version = "1.18.1";
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "localsend";
+    repo = "localsend";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-uxMfO39U0cC7Svf8QL4WUJJowuwmTjChrZ4VOWcgb4E=";
+  };
+
+  cargoHash = "sha256-khnrHc/88j+JG3sSKvNUsu6JcrM83zIt+W3tT7dz2Ac=";
+
+  cargoBuildFlags = [
+    "--package"
+    "localsend-cli"
+  ];
+
+  checkInputs = [
+    cacert
+  ];
+
+  doCheck = !stdenv.hostPlatform.isDarwin;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Open source cross-platform alternative to AirDrop (cli version)";
+    homepage = "https://github.com/localsend/localsend";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ zendo ];
+    mainProgram = "localsend-cli";
+  };
+})
